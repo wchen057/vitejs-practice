ec05c08bc6919377744e4546619ddb5a6bdcf3d1
Ritch Chan <ritch.chan@hotmail.com>
2022-12-27 10:21:36 -0500

    ol original vitejs

--- main.js
+++ main.js
@@ -1,82 +1,17 @@
-import 'ol/ol.css';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import {
-  DragAndDrop,
-  defaults as defaultInteractions,
-} from 'ol/interaction';
-import {GPX, GeoJSON, IGC, KML, TopoJSON} from 'ol/format';
-import {
-  Tile as TileLayer,
-  VectorImage as VectorImageLayer,
-} from 'ol/layer';
-import {Vector as VectorSource, XYZ} from 'ol/source';
-
-const dragAndDropInteraction = new DragAndDrop({
-  formatConstructors: [GPX, GeoJSON, IGC, KML, TopoJSON],
-});
-
-const key = 'Get your own API key at https://www.maptiler.com/cloud/';
-const attributions =
-  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
-  '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
 
 const map = new Map({
-  interactions: defaultInteractions().extend([dragAndDropInteraction]),
+  target: 'map',
   layers: [
     new TileLayer({
-      source: new XYZ({
-        attributions: attributions,
-        url:
-          'https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=' + key,
-        maxZoom: 20,
-      }),
-    }),
+      source: new OSM()
+    })
   ],
-  target: 'map',
   view: new View({
     center: [0, 0],
-    zoom: 2,
-  }),
-});
-
-dragAndDropInteraction.on('addfeatures', function (event) {
-  const vectorSource = new VectorSource({
-    features: event.features,
-  });
-  map.addLayer(
-    new VectorImageLayer({
-      source: vectorSource,
-    })
-  );
-  map.getView().fit(vectorSource.getExtent());
-});
-
-const displayFeatureInfo = function (pixel) {
-  const features = [];
-  map.forEachFeatureAtPixel(pixel, function (feature) {
-    features.push(feature);
-  });
-  if (features.length > 0) {
-    const info = [];
-    let i, ii;
-    for (i = 0, ii = features.length; i < ii; ++i) {
-      info.push(features[i].get('name'));
-    }
-    document.getElementById('info').innerHTML = info.join(', ') || '&nbsp';
-  } else {
-    document.getElementById('info').innerHTML = '&nbsp;';
-  }
-};
-
-map.on('pointermove', function (evt) {
-  if (evt.dragging) {
-    return;
-  }
-  const pixel = map.getEventPixel(evt.originalEvent);
-  displayFeatureInfo(pixel);
-});
-
-map.on('click', function (evt) {
-  displayFeatureInfo(evt.pixel);
+    zoom: 2
+  })
 });
